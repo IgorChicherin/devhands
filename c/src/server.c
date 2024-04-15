631286e212c97427915578629978e601d858630e
Ingwar <ashzak990@gmail.com>
2024-04-15 16:41:49 +0300

    epoll model

--- c/src/server.c
+++ c/src/server.c
@@ -1,11 +1,30 @@
-#include "server.h"
 #include <netinet/in.h>
 #include <stdbool.h>
-#include <stdlib.h>
+
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#include "server.h"
 
 char buffer[BUFFER_SIZE];
+
+int setnonblocking(int sockfd) {
+	if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) | O_NONBLOCK) ==-1) {
+		return -1;
+	}
+	return 0;
+}
+
+void epoll_ctl_add(int epfd, int fd, uint32_t events) {
+	struct epoll_event ev;
+	ev.events = events;
+	ev.data.fd = fd;
+	if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+		perror("epoll_ctl()\n");
+		exit(1);
+	}
+}
 
 Server server_constructor(int domain, int service, int protocol,
                           u_long interface, int port, int backlog,
@@ -32,6 +51,14 @@ Server server_constructor(int domain, int service, int protocol,
     exit(1);
   }
 
+  
+  // Allow reuse sockets
+  int yes = 1;
+  if (setsockopt(server.socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("Setsockopt error");
+        exit(1);
+  } 
+
   // Binding socket with address
   int res = bind(server.socket, (struct sockaddr *)&server.address,
                  sizeof(server.address));
@@ -39,6 +66,13 @@ Server server_constructor(int domain, int service, int protocol,
   if (res < 0) {
     perror("Failed to bind socket");
     exit(1);
+  }
+
+  // Set sock non blocking
+
+  if (setnonblocking(server.socket) == -1) {
+    perror("Failed set socket non-blocking mode");
+    exit(-1);
   }
 
   // Listen connections
@@ -49,6 +83,17 @@ Server server_constructor(int domain, int service, int protocol,
     exit(1);
   }
 
+ // create epoll instance
+  server.epfd = epoll_create(5);
+  if (server.epfd == -1) {
+      perror("epoll_create");
+  }
+
+  struct epoll_event ev;
+  memcpy(&ev.events, server.events, sizeof(server.events));
+	ev.data.fd = server.epfd;
+
+  epoll_ctl_add(server.epfd, server.socket, EPOLLIN | EPOLLOUT | EPOLLET);
   server.launch = launch;
 
   memcpy(&server.views, views, sizeof(ViewsList));
@@ -56,73 +101,102 @@ Server server_constructor(int domain, int service, int protocol,
 }
 
 void serve(Server *server) {
+  
   for (;;) {
-    // Accept incoming connections
-    int host_len = sizeof(server->address);
-    int newsockfd = accept(server->socket, (struct sockaddr *)&server->address,
-                           (socklen_t *)&host_len);
 
-    if (newsockfd < 0) {
-      perror("webserver (accept)");
-      continue;
-    }
+    int count_fds = epoll_wait(server->epfd, server->events, MAX_EVENTS, -1);
 
-    // Get client address
-    struct sockaddr_in client;
-    int client_len = sizeof(client);
-    int sockn = getsockname(newsockfd, (struct sockaddr *)&client,
-                            (socklen_t *)&client_len);
+    for (int i = 0 ; i < count_fds; ++i) {
 
-    if (sockn < 0) {
-      perror("webserver (getsockname)");
-      continue;
-    }
+      // Handle connection
+      if (server->events[i].data.fd == server->socket) {
+          // Accept incoming connections
+          int host_len = sizeof(server->address);
+          int newsockfd = accept(server->socket, (struct sockaddr *)&server->address,
+                                (socklen_t *)&host_len);
 
-    // Read from the socket
-    int valread = read(newsockfd, buffer, BUFFER_SIZE);
-    if (valread < 0) {
-      continue;
-    }
-
-    // Read the request
-    Request req;
-    req.sockfd = newsockfd;
-
-    char version[BUFFER_SIZE];
-    sscanf(buffer, "%s %s %s", req.method, req.path, version);
-
-    printf("%s:%u [%s] %s %s\n", inet_ntoa(client.sin_addr),
-           ntohs(client.sin_port), req.method, version, req.path);
-
-    bool view_found = false;
-    for (int i = 0; i < server->views.views_count && !view_found; i++) {
-      View view = server->views.views[i];
-
-      if (strcmp(req.path, view.path) == 0) {
-
-        for (int j = 0; j < view.handlers_count; j++) {
-          Handler handler = view.handlers[j];
-
-          if (strcmp(req.method, handler.method) == 0) {
-            view_found = true;
-            handler.func(&req);
-            close(req.sockfd);
-            break;
+          if (newsockfd < 0) {
+            perror("webserver (accept)");
+            continue;
           }
-        }
+
+          setnonblocking(newsockfd);
+          epoll_ctl_add(server->epfd, newsockfd, EPOLLIN | EPOLLET | EPOLLRDHUP | EPOLLHUP);
       }
+      
+      if (server->events[i].events & EPOLLIN) {
+        // Read from the socket
+        int valread = read(server->events[i].data.fd, buffer, BUFFER_SIZE);
+        if (valread < 0) {
+          continue;
+        }
+
+
+        // Get client address
+        struct sockaddr_in client;
+        int client_len = sizeof(client);
+        int sockn = getsockname(server->events[i].data.fd, (struct sockaddr *)&client,
+                                (socklen_t *)&client_len);
+
+        if (sockn < 0) {
+          perror("webserver (getsockname)");
+          continue;
+        }
+
+        // Read the request
+        Request req;
+        req.sockfd = server->events[i].data.fd;
+
+        char version[BUFFER_SIZE];
+        sscanf(buffer, "%s %s %s", req.method, req.path, version);
+
+        printf("%s:%u [%s] %s %s\n", inet_ntoa(client.sin_addr),
+              ntohs(client.sin_port), req.method, version, req.path);
+
+        route_view(server, &req);
+    
+      }
+
+      /* check if the connection is closing */
+			if (server->events[i].events & (EPOLLRDHUP | EPOLLHUP)) {
+				printf("[+] connection closed\n");
+				epoll_ctl(server->epfd, EPOLL_CTL_DEL, server->events[i].data.fd, NULL);
+				close(server->events[i].data.fd);
+				continue;
+			}
     }
-    if (!view_found) {
+  }
+}
+
+void route_view(Server *server, Request *req) {
+      bool view_found = false;
+        for (int i = 0; i < server->views.views_count && !view_found; i++) {
+          View view = server->views.views[i];
+
+          if (strcmp(req->path, view.path) == 0) {
+
+            for (int j = 0; j < view.handlers_count; j++) {
+              Handler handler = view.handlers[j];
+
+              if (strcmp(req->method, handler.method) == 0) {
+                view_found = true;
+                handler.func(req);
+                close(req->sockfd);
+                break;
+              }
+            }
+          }
+        }     
+
+      if (!view_found) {
       char resp_404[] = "HTTP/1.1 404 Not Found\r\n"
                         "Server: webserver-c\r\n"
                         "Content-type: text/html\r\n\r\n"
                         "<html>Page not found</html>\r\n";
 
-      int valwrite = write(req.sockfd, resp_404, strlen(resp_404));
+      int valwrite = write(req->sockfd, resp_404, strlen(resp_404));
       if (valwrite < 0) {
         perror("webserver (write)");
       }
-      close(req.sockfd);
     }
-  }
 }
--- c/src/server.h
+++ c/src/server.h
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
 
 #include <errno.h>
 #include <stdio.h>
@@ -13,6 +15,13 @@
 #define BUFFER_SIZE 4096
 #endif // !BUFFER_SIZE
 
+#ifndef MAX_EVENTS
+#define MAX_EVENTS 1
+#endif // !MAX_EVENTS
+
+int setnonblocking(int sockfd);
+void epoll_ctl_add(int epfd, int fd, uint32_t events);
+
 typedef struct Server {
   int domain;
   int service;
@@ -24,9 +33,13 @@ typedef struct Server {
   int socket;
   void (*launch)(struct Server *server);
   int views_count;
+  int epfd;
+  struct epoll_event events[MAX_EVENTS];
   ViewsList views;
 } Server;
 
 Server server_constructor(int domain, int service, int protocol,
                           u_long interface, int port, int backlog,
                           void (*launch)(Server *server), ViewsList *views);
+
+void route_view(Server *server, Request *req);
